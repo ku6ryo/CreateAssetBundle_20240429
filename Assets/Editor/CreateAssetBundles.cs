b39d44ba361a3e9e59904d8cda6443c59146faf4
Ryo Kuroyanagi <im.ryokuro@gmail.com>
2024-05-05 02:14:10 +0900

    First commit

---
+++ Assets/Editor/CreateAssetBundles.cs
@@ -0,0 +1,60 @@
+using UnityEditor;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class CreateAssetBundles
+{
+    [MenuItem("Assets/Build asset bundle for VISIONIAR", validate = true)]
+    static bool ValidateDoSomethingWithSelectedObjects()
+    {
+        // This method determines whether the menu item should be enabled or not
+        return Selection.objects != null && Selection.objects.Length > 0;
+    }
+
+    [MenuItem("Assets/Build asset bundle for VISIONIAR")]
+    static void BuildAssetBundles()
+    {
+        var selectedObj = Selection.activeObject;
+        if (selectedObj == null) {
+            Debug.LogError("No object selected");
+        }
+        // Check if it is a prefab
+        var isPrefab = PrefabUtility.GetPrefabAssetType(selectedObj) == PrefabAssetType.Regular;
+        if (isPrefab) {
+            string path = AssetDatabase.GetAssetPath(selectedObj);
+            Debug.Log($"Building an asset bundle for {path}");
+            var uuid = Guid.NewGuid().ToString();
+            var goNameOfPrefab = selectedObj.name;
+            selectedObj.name = uuid;
+            var filenameAndroid = $"{uuid}.android";
+            var filenameIOS = $"{uuid}.ios";
+            var filenameWin = $"{uuid}.windows";
+            var filenameMac = $"{uuid}.mac";
+            try {
+                BuildAssetBundleForTarget(path, filenameAndroid, BuildTarget.Android);
+                BuildAssetBundleForTarget(path, filenameIOS, BuildTarget.iOS);
+                BuildAssetBundleForTarget(path, filenameWin, BuildTarget.StandaloneWindows);
+                BuildAssetBundleForTarget(path, filenameMac, BuildTarget.StandaloneOSX);
+            } finally {
+                selectedObj.name = goNameOfPrefab;
+            }
+        } else {
+            Debug.Log("Selected object is not a regular prefab");
+        }
+    }
+
+    static void BuildAssetBundleForTarget(string assetPath, string filename, BuildTarget target)
+    {
+        AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
+        buildMap[0].assetBundleName = filename;
+        buildMap[0].assetNames = new string[] { assetPath };
+        var outputDir = $"AssetBundles";
+        // Check if the output directory exists
+        if (!Directory.Exists(outputDir))
+        {
+            Directory.CreateDirectory(outputDir);
+        }
+        BuildPipeline.BuildAssetBundles(outputDir, buildMap, BuildAssetBundleOptions.None, target);
+    }
+}
